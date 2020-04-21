eb1f05932f1984e862ae1a08119d626c2841ed10
Nicolas Juillet <juillet.n@hotmail.fr>
2020-04-21 17:00:36 +0100

    Ajout d'une barre de navigateur, d'un fichier synopsis + CSS frontend

--- Essai_projet/PHP/view/front/lastNews.php
+++ Essai_projet/PHP/view/front/lastNews.php
@@ -3,9 +3,8 @@
     <div class="blocpage">
         <head>
             <title>Billet simple pour l'Alaska</title>
-            <meta charset="utf-8">
-            <link href="../../CSS/frontend/frontend.css" rel="stylesheet" />
-            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="../../CSS/frontend/frontend.css" />
+            <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
             <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         </head>
 
@@ -15,13 +14,13 @@
                     <div class="col-lg-12">
                         <nav id="summary">
                             <ul>
-                                <li><button id="biography">Biographie</a></li>
+                                <li><button id="synopsis">Synopsis</a></li>
                                 <li><button id="home">Accueil</a></li>
                                 <li><button id="last-episodes">Derniers épisodes</a></li>
                                 <li><input type="search" id="site-search" name="q" aria-label="Search through site content"></li>
                                 <li><button id="register">S'inscrire</button></li>
                                 <li><button id="connection">Se connecter</button></li>
-                                <li><p>Bienvenue <?= $user->userId()?></p></li>
+                                <!-- <li><p>Bienvenue <//?= $user->userId()?></p></li> -->
                                 <li><button id="logout">Se déconnecter</button></li>
                             </ul>
                         </nav>
---
+++ Essai_projet/PHP/view/front/synopsis.php
@@ -0,0 +1,23 @@
+<html>
+    <div class="blocpage">
+        <head>
+            <title>Billet simple pour l'Alaska</title>
+            <meta charset="utf-8">
+            <link href="../../CSS/frontend/frontend.css" rel="stylesheet" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        </head>
+
+        <body>
+            <div id="synopsis-content">
+            <h1>Résumé de l'histoire :</h1>
+            <p>CONTENU</p>
+            </div>
+
+            <div id="biography">
+            <h1>Biographie de l'auteur</h1>
+            <p>Bonjour, je suis Jean Forteroche, écrivain.</p>
+            </div>
+        </body>
+    </div>
+</html>
