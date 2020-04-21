2fb5bc297ca8094934888b68985f11b705562fb7
Nicolas Juillet <juillet.n@hotmail.fr>
2020-04-21 12:58:33 +0100

    Travail sur bootstrap, ajout d'une barre de navigation + CSS

--- Essai_projet/PHP/view/front/lastNews.php
+++ Essai_projet/PHP/view/front/lastNews.php
@@ -10,26 +10,43 @@
         </head>
 
         <body>
-
             <header>
-                <h1 id="title">Billet simple pour l'Alaska</h1>
-                <h2 id="author">Par Jean Forteroche</h2>
+                <section class="row">
+                    <div class="col-lg-12">
+                        <nav id="summary">
+                            <ul>
+                                <li><button id="biography">Biographie</a></li>
+                                <li><button id="home">Accueil</a></li>
+                                <li><button id="last-episodes">Derniers épisodes</a></li>
+                                <li><input type="search" id="site-search" name="q" aria-label="Search through site content"></li>
+                                <li><button id="register">S'inscrire</button></li>
+                                <li><button id="connection">Se connecter</button></li>
+                                <li><p>Bienvenue <?= $user->userId()?></p></li>
+                                <li><button id="logout">Se déconnecter</button></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </section>
+
+                <section class="row">
+                    <div class="col-lg-12">
+                        <h1 id="title">Billet simple pour l'Alaska</h1>
+                        <h2 id="author">Par Jean Forteroche</h2>
+                    </div>
+                </section>
             </header>
 
-            <section id="newsPreview">
-
-            <div class="col-md-8">
+                <section class="row">
             <?php
                 foreach($news as $showNews)
             { ?>
+                <div class="col-md-4 col-md-offset-2">
                 <h1 class="newsTitlePreview"><?= $showNews->title()?></h1>
                 <article class="newsContentPreview"><?= $showNews->content()?></article>
                 <button class="showNewsButton">Lire la suite</button>
+                </div>
             <?php } ?>
-            </div>
-
-            </section>
-
+                </section>
         </body>
     </div>
 </html>
