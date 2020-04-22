ca02d929d1f51c3f87de34c56aee956503c69c6c
Nicolas Juillet <juillet.n@hotmail.fr>
2020-04-22 10:39:48 +0200

    ajout de CSS, amélioration de la page d'un épisode entier et ajout d'une image fixe en guise de fond d'écran

--- Essai_projet/PHP/view/front/lastNews.php
+++ Essai_projet/PHP/view/front/lastNews.php
@@ -10,13 +10,14 @@
 
         <body>
             <header>
+                <img src=../image-fond.jpeg id=background-image class=background-image alt="background image"/>
                 <section class="row">
                     <div class="col-lg-12">
                         <nav id="summary">
                             <ul>
-                                <li><button id="synopsis">Synopsis</a></li>
-                                <li><button id="home">Accueil</a></li>
-                                <li><button id="last-episodes">Derniers épisodes</a></li>
+                                <li><button id="synopsis">Synopsis</li>
+                                <li><button id="home">Accueil</li>
+                                <li><button id="last-episodes">Derniers épisodes</li>
                                 <li><input type="search" id="site-search" name="q" aria-label="Search through site content"></li>
                                 <li><button id="register">S'inscrire</button></li>
                                 <li><button id="connection">Se connecter</button></li>
@@ -29,23 +30,37 @@
 
                 <section class="row">
                     <div class="col-lg-12">
-                        <h1 id="title">Billet simple pour l'Alaska</h1>
-                        <h2 id="author">Par Jean Forteroche</h2>
+                        <div id="title-framed">
+                            <h1 id="title">Billet simple pour l'Alaska</h1>
+                            <h2 id="author">Par Jean Forteroche</h2>
+                        </div>
                     </div>
                 </section>
             </header>
 
-                <section class="row">
-            <?php
-                foreach($news as $showNews)
-            { ?>
-                <div class="col-md-4 col-md-offset-2">
-                <h1 class="newsTitlePreview"><?= $showNews->title()?></h1>
-                <article class="newsContentPreview"><?= $showNews->content()?></article>
-                <button class="showNewsButton">Lire la suite</button>
-                </div>
-            <?php } ?>
+                <section id="news-preview-bloc">
+                    <section class="row">
+                    <?php
+                        foreach($news as $showNews)
+                    { ?>
+                        <div class="col-md-offset-2 col-md-4">
+                            <div id="news-preview">
+                                <h1 class="newsTitlePreview"><?= $showNews->title()?></h1>
+                                <article class="newsContentPreview"><?= $showNews->content()?></article>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    </section>
                 </section>
+                <footer>
+                    <div id="next-page">
+                        <ul>
+                            <li><button>1</button></li>
+                            <li><button>2</button></li>
+                            <li><button>3</button></li>
+                        </ul>
+                    </div>
+                </footer>
         </body>
     </div>
 </html>
