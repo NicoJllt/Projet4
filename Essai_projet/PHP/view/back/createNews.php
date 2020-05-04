e7282b8f0724763e828c9943d0cdfc57c66e4c26
Nicolas Juillet <juillet.n@hotmail.fr>
2020-05-04 19:47:00 +0200

    intégration de TinyMCE au formulaire de création d'un épisode

--- Essai_projet/PHP/view/back/createNews.php
+++ Essai_projet/PHP/view/back/createNews.php
@@ -8,6 +8,8 @@
         <link rel="stylesheet" href="../../CSS/backend/backend.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">        
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script> 
+        <script>tinymce.init({ selector:'textarea' });</script>
     </head>
 
     <body>
